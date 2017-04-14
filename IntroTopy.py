bf7b41282cc9f0406811266781320a33c6242441
ys8574 <ys_8574@aswinnen.be>
2017-04-14 11:17:16 +0200

    I have added animal class to MyModule

--- IntroTopy.py
+++ IntroTopy.py
@@ -1,2 +1,5 @@
 print("Hello World")
 print("This is the best test ever of the universe")
+import pandas as pd
+s=pd.Series([1,2,3])
+print(s)
---
+++ MyModule.py
@@ -0,0 +1,7 @@
+class animal(object):
+    def __init__(self, startname,age):
+        self.name = startname # attribute
+        self.age = age #attribute2
+    def description(selfself): #method1
+        print("this is " + self.name)
+        print("he/she is " + str(self.age)+"years old")
