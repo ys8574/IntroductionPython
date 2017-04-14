d53a23d527184a90693beae10cc3ccab3613e8ea
ys8574 <ys_8574@aswinnen.be>
2017-04-14 13:26:59 +0200

    Pandas created

--- IntroTopy.py
+++ IntroTopy.py
@@ -1,5 +1,46 @@
-print("Hello World")
-print("This is the best test ever of the universe")
-import pandas as pd
-s=pd.Series([1,2,3])
-print(s)
+# from MyModule import animal
+# zebra = animal("Samba", 4)
+# elephant = animal("Dumbo", 2)
+# zebra.description()
+# elephant.description()
+# import math
+# print (math.sqrt(36))
+# #same way:
+# from math import sqrt
+# print(sqrt(36))
+# #import all functions from math
+# from math import *
+# print(acos(1))
+# #can be dangerous to call all functions by using "*", you can have defined the same name for something else (as shown below)
+# def sqrt (number):
+#     print("bla")
+# sqrt(36)
+from joblib import parallel, delayed
+import multiprocessing
+import time
+
+
+# def myfunction(i):
+#     print (i)
+#     time.sleep(2)
+#     return (i)
+# if __name__=='__main__':
+#     start=time.time()
+#     for i in range(10):
+#         myfunction(i)
+#     print("Serial",time.time()-start)
+
+def myfunction(i):
+    print (i)
+    time.sleep(2)
+    return (i)
+if __name__=='__main__':
+    start=time.time()
+    for i in range(10):
+        myresults = myfunction(i)
+    print("Serial",time.time()-start)
+    num_cores = multiprocessing.cpu_count()
+    print("Number of cores used." + str(num_cores))
+    results=parallel(n_jobs=num_cores)(delayed(myfunction)(i) for i in range(10))
+    print("Parallel",time.time() -start)
+    print(results)
--- MyModule.py
+++ MyModule.py
@@ -2,6 +2,6 @@ class animal(object):
     def __init__(self, startname,age):
         self.name = startname # attribute
         self.age = age #attribute2
-    def description(selfself): #method1
-        print("this is " + self.name)
-        print("he/she is " + str(self.age)+"years old")
+    def description(self): #method1
+        print("This is " + self.name)
+        print("He/She is " + str(self.age)+" years old! HUBA HUBA")
